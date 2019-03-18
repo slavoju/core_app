56f86ec6f55a02c557eec9c3ced63e31b603c5f0
slavoju <github@slavo.pl>
2019-03-18 16:02:04 +0100

    switch to .Net Core 2.0

--- core_app/Controllers/ValuesController.cs
+++ core_app/Controllers/ValuesController.cs
@@ -7,19 +7,18 @@ using Microsoft.AspNetCore.Mvc;
 namespace core_app.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
+     public class ValuesController : ControllerBase
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public string Get(int id)
         {
             return "value";
         }
